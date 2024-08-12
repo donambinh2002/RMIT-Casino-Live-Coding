31bc038138b58437a1fcb05e36ca481a50d41d20
Do Nam Binh <s3878089@rmit.edu.vn>
2024-08-12 09:44:50 +0700

    Initial Commit

---
+++ RMIT Casino Live Coding/RMIT_Casino_Live_CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Casino_Live_CodingApp.swift
+//  RMIT Casino Live Coding
+//
+//  Created by Binh on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Casino_Live_CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
